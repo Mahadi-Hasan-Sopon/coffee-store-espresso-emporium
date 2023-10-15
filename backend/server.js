d9a00115d78690f18e0e7b7d7acf8e32f8186d32
Mahadi Hasan <mahadih137@gmail.com>
2023-10-16 00:07:04 +0600

    server side routing created

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,57 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const coffeeCollection = client.db("coffeesDB").collection("coffees");
+
     //   routes
 
     app.get("/", (req, res) =>
       res.send("<h1><center>Hello from server</center></h1>")
-    ); 
+    );
+
+    app.get("/coffees", async (req, res) => {
+      const coffees = await coffeeCollection.find().toArray();
+      res.send(coffees);
+    });
+
+    app.post("/coffees/new", async (req, res) => {
+      const coffee = req.body;
+      const result = await coffeeCollection.insertOne(coffee);
+      res.send(result);
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    });
+
+    app.put("/coffees/:id", async (req, res) => {
+      const id = req.params.id;
+      const coffee = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCoffee = {
+        $set: {
+          ...coffee,
+        },
+      };
+      const result = await coffeeCollection.updateOne(
+        filter,
+        updatedCoffee,
+        options
+      );
+      console.log(
+        `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
+      );
+    });
+
+    app.delete("/coffees/:id", async (req, res) => {
+      const coffeeId = req.params.id;
+      const query = { _id: new ObjectId(coffeeId) };
+      const result = await coffeeCollection.deleteOne(query);
+      if (result.deletedCount === 1) {
+        console.log("Successfully deleted one document.");
+      } else {
+        console.log("No documents matched the query. Deleted 0 documents.");
+      }
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
